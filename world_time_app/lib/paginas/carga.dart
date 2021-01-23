1e0ef0ce938cd5560e62d33d19a39618ac34f8c9
Nicolas Lopez <nlopez97f@gmail.com>
2021-01-23 18:00:14 -0500

    Paginas done

---
+++ world_time_app/lib/paginas/carga.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Carga extends StatefulWidget {
+  @override
+  _CargaState createState() => _CargaState();
+}
+
+class _CargaState extends State<Carga> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Pagina de carga'),
+    );
+  }
+}
---
+++ world_time_app/lib/paginas/ubicacion.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Ubicacion extends StatefulWidget {
+  @override
+  _UbicacionState createState() => _UbicacionState();
+}
+
+class _UbicacionState extends State<Ubicacion> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Pagina de ubicacion'),
+    );
+  }
+}
