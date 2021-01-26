35c94cde305a6817e6d69e319716edf2ca101cbb
Nicolas Lopez <nlopez97f@gmail.com>
2021-01-26 17:13:48 -0500

    Componentes, first commit

---
+++ componentes/lib/src/routes/routes.dart
@@ -0,0 +1,12 @@
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+Map<String, WidgetBuilder> getApplicationRoutes() {
+  return <String, WidgetBuilder>{
+    '/': (context) => HomePage(),
+    'alert': (context) => AlertPage(),
+    'avatar': (context) => AvatarPage(),
+  };
+}
